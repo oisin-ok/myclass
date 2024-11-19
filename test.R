d3377219832d2d0ae25a4f1e48142b5f7bec89bb
Oisin OK <oisin66@gmail.com>
2024-11-19 14:35:29 +0000

    added Shiny run direct code

--- test.R
+++ test.R
@@ -1,3 +1,14 @@
 #Test 123
 
 #changing on GitHub now :D
+
+#running Shiny app directly from GitHub
+runGitHub(repo = "myclass-repo",
+          username = "davoodroshan",
+          ref="main",
+          subdir="App")
+
+runGitHub(repo = "myclass",
+          username = "oisin-ok",
+          ref="main",
+          subdir="App")
